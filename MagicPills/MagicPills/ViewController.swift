7716787976f812818ca93996a2eb6ab3f5c76755
Jude Martin <judemartin.mj@gmail.com>
2016-09-04 16:41:37 -0700

    Magic Pills App

---
+++ MagicPills/MagicPills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicPills
+//
+//  Created by MARTIN JUDE on 9/4/16.
+//  Copyright © 2016 MARTINJUDE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
